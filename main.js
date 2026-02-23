a1110c4f6c4cf01412fd447fd7f9543867147781
CVT17 <158773525+CVT17@users.noreply.github.com>
2026-02-24 00:45:07 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//document.getElementById("one").innerHTML = ("New str")
+let btn = document.querySelector(".chek-in")
+let inp = document.querySelector(".text")
+btn.onclick = function() {
+  console.log(inp.value)
+}
+
+
+let (a + b) = 15
+
+if (a > 15) {
+    console.log("Да")
+}
+else if (a < 15) 
+    console.log("Нет");
+
+else {
+    console.log("Нет")
+}
+
+
+let a = 5
+let b = 10
+if (a == 5 || b == 10) {
+console.log(true);
+ }else {
+    console.log(false);
+}
