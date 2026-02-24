0be83df8456780cebc5bbe696f42a429115ed9db
CVT17 <158773525+CVT17@users.noreply.github.com>
2026-02-25 02:39:52 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-//document.getElementById("one").innerHTML = ("New str")
-let btn = document.querySelector(".chek-in")
-let inp = document.querySelector(".text")
-btn.onclick = function() {
-  console.log(inp.value)
-}
 
-
-let (a + b) = 15
-
-if (a > 15) {
+let a = prompt("Введите число")
+let (a) = 15  
+  if (a > 15) {
     console.log("Да")
 }
-else if (a < 15) 
-    console.log("Нет");
 
-else {
+  else if(a < 15){
+    console.log("Нет")
+}
+
+  else {
     console.log("Нет")
 }
 
 
-let a = 5
+
+let y = 5
 let b = 10
-if (a == 5 || b == 10) {
+if (y == 5 || b == 10) {
 console.log(true);
  }else {
     console.log(false);
